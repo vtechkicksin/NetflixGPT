9d465f8cc4a3006b8d786c8f720f0ad9da191d5d
Sandeep Kumar <sandeepkumar@Mac.lan>
2025-12-22 17:01:12 +0530

    added sign up field

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -11,12 +11,12 @@ const Login = () => {
             alt='logo'/>
       </div>
       <form className='w-3/12 absolute p-12 bg-black/75 my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80'>
-        <h1>Sign In</h1>
-        {signUp && <input type='text' placeholder='Name' className='p-4 my-6 w-full bg-gray-700 rounded-lg'/> }
+        <h1>{signUp ? "Sign Up" : "Sign In"}</h1>
+        {signUp && <input type='text' placeholder='Full Name' className='p-4 my-6 w-full bg-gray-700 rounded-lg'/> }
         <input type='text' placeholder='Email Address' className='p-4 my-6 w-full bg-gray-700 rounded-lg'/>
         <input type='password' placeholder='Password' className='p-4 my-6 w-full bg-gray-700 rounded-lg'/>
-        <button className='p-4 my-6 bg-red-400 w-full rounded-lg'>Submit</button>
-        <p className='py-4 cursor-pointer' onClick={()=>setSignUp(prev=>!prev)}>New to Netflix ? {signUp ? "Sign In" : "Sign Up"}</p>
+        <button className='p-4 my-6 bg-red-400 w-full rounded-lg'>{signUp ? "Sign Up" : "Sign In"}</button>
+        <p className='py-4 cursor-pointer' onClick={()=>setSignUp(prev=>!prev)}> {!signUp ? "New to Netflix ? Sign Up" : "Already registered ? Sign In Now"}</p>
       </form>
     </div>
   )
